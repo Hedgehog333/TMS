eb4fb58f5fddbe7fd6ef0b65acee52697218f3b6
Hedgehog333 <artem1100.94@gmail.com>
2016-04-20 02:29:11 +0300

    add DAO interface

---
+++ TMS/dao/IDAO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS.dao
+{
+    interface IDAO<T>
+        where T : class
+    {
+        T get(int id);
+        List<T> getAll();
+        T create(T item);
+        void update(T item);
+        void delete(T item);
+    }
+}
