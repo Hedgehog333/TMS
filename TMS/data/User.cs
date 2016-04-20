ecbff9ca9e5c47714818f6f64d1bfe88f9805ba3
Hedgehog333 <artem1100.94@gmail.com>
2016-04-20 03:15:05 +0300

    add User

---
+++ TMS/data/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TMS.data
+{
+    public class User
+    {
+        int id { set; get; }
+        string fName { set; get; }
+        string lName { set; get; }
+        string sName { set; get; }
+        dao.ERoles role { set; get; }
+        DateTime registrationDate { set; get; }
+        DateTime lastOnlineDate { set; get; }
+        
+    }
+}
