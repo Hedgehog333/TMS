f8dd386c97a2dccb0d7dd079238a2ec992b2eb7c
Hedgehog333 <artem1100.94@gmail.com>
2016-04-20 03:24:05 +0300

    add Answer

---
+++ TMS/data/Answer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS.data
+{
+    class Answer
+    {
+        bool isTrue { set; get; }
+        string body { set; get; }
+    }
+}
