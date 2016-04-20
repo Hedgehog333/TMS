4dc85cd4df2ee6cb9c3acbc1374de4f3b0da4bb6
Hedgehog333 <artem1100.94@gmail.com>
2016-04-20 03:36:03 +0300

    add Quetion

---
+++ TMS/data/Question.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS.data
+{
+    class Question
+    {
+        public bool isFowAnswers { set; get; }
+        public string body { set; get; }
+        public List<data.Answer> answers;
+        public Question()
+        {
+            this.answers = new List<Answer>();
+        }
+    }
+}
