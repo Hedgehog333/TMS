af685e10d9575f38abed91558034e8618883ac73
Hedgehog333 <artem1100.94@gmail.com>
2016-04-24 05:15:01 +0300

    add toString

--- TMS/data/User.cs
+++ TMS/data/User.cs
@@ -12,5 +12,35 @@ namespace TMS.data
         public DateTime registrationDate { set; get; }
         public DateTime lastOnlineDate { set; get; }
         
+        public User
+            (
+                int id,
+                string fName,
+                string lName,
+                string sName,
+                dao.ERoles role,
+                DateTime registrationDate,
+                DateTime lastOnlineDate
+            )
+        {
+            this.id = id;
+            this.fName = fName;
+            this.lName = lName;
+            this.sName = sName;
+            this.role = role;
+            this.registrationDate = registrationDate;
+            this.lastOnlineDate = lastOnlineDate;
+        }
+
+        public override string ToString()
+        {
+            return "id: " + this.id +
+                "\nfirst name: " + this.fName +
+                "\nlast name: " + this.lName +
+                "\nsecond name: " + this.sName +
+                "\nrole: " + this.role.ToString() +
+                "\nregistration date: " + this.registrationDate +
+                "\nlast online date: " + this.lastOnlineDate;
+        }
     }
 }
