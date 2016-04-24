6a642d8d44ad58164afd91f1d773725864fc736f
Hedgehog333 <artem1100.94@gmail.com>
2016-04-24 05:24:52 +0300

    create

--- TMS/dao/IDAO.cs
+++ TMS/dao/IDAO.cs
@@ -11,7 +11,7 @@ namespace TMS.dao
     {
         T get(int id);
         List<T> getAll();
-        T create(T item);
+        void add(T item);
         void update(T item);
         void delete(T item);
     }
