7eb531a6a93904d94024d5aa16098535958cc2aa
Hedgehog333 <artem1100.94@gmail.com>
2016-04-25 11:40:22 +0300

    add field

--- TMS/data/Task.cs
+++ TMS/data/Task.cs
@@ -8,6 +8,8 @@ namespace TMS.data
 {
     class Task
     {
+        public int id { set; get; }
+        public string title { set; get; }
         public List<data.Question> question = new List<Question>();
         public Task()
         {
