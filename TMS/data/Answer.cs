5422dc2a616954df80c2b79e309f7afc74157cbf
Hedgehog333 <artem1100.94@gmail.com>
2016-05-01 21:06:11 +0300

    add field & rename

--- TMS/data/Answer.cs
+++ TMS/data/Answer.cs
@@ -9,21 +9,24 @@ namespace TMS.data
     class Answer
     {
         public int id { set; get; }
-        public bool isTrue { set; get; }
         public string body { set; get; }
+        public bool isCorrect { set; get; }
         public int questionId { set; get; }
+        public bool isDraft { set; get; }
         public Answer
             (
                 int id,
-                bool isTrue,
                 string body,
-                int questionId
+                bool isTrue,
+                int questionId,
+                bool isDraft
             )
         {
             this.id = id;
-            this.isTrue = isTrue;
             this.body = body;
+            this.isCorrect = isCorrect;
             this.questionId = questionId;
+            this.isDraft = isDraft;
         }
     }
 }
