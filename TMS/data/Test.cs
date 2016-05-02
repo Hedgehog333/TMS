e8a1edda83e91e602e0e1b0d00d3bb41ca42b533
Hedgehog333 <artem1100.94@gmail.com>
2016-05-02 22:21:08 +0300

    Test database works classes & edit name xml files

---
+++ TMS/data/Test.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS.data
+{
+    class Test
+    {
+        public int id { set; get; }
+        public string title { set; get; }
+        public string desctiption { set; get; }
+        public int categoriesId { set; get; }
+        public DateTime creationDate { set; get; }
+        public int authorId { set; get; }
+        public bool isDraft { set; get; }
+        public List<data.Question> question = new List<Question>();
+        public Test
+            (
+                int id,
+                string title,
+                string desctiption,
+                int categoriesId,
+                DateTime creationDate,
+                int authorId,
+                bool isDraft
+            )
+        {
+            this.id = id;
+            this.title = title;
+            this.desctiption = desctiption;
+            this.categoriesId = categoriesId;
+            this.creationDate = creationDate;
+            this.authorId = authorId;
+            this.isDraft = isDraft;
+            this.question = new List<Question>();
+        }
+    }
+}
---
+++ TMS/db/XMLTestDB.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace TMS.db
+{
+    class XMLTestDB : dao.IDAO<data.Test>
+    {
+        public data.Test get(int id)
+        {
+            IsFileExists();
+
+            XDocument doc = XDocument.Load(ConfigurationManager.AppSettings["TestsFile"]);
+            var task = (from T in doc.Root.Elements("test")
+                          where Int32.Parse(T.Attribute("id").Value) == id
+                          select new data.Test
+                              (
+                                 Int32.Parse(T.Attribute("id").Value),
+                                 T.Element("title").Value,
+                                 T.Element("desctiption").Value,
+                                 Int32.Parse(T.Attribute("categoriesId").Value),
+                                 DateTime.Parse(T.Element("creationDate").Value),
+                                 Int32.Parse(T.Attribute("authorId").Value),
+                                 Boolean.Parse(T.Element("isDraft").Value)
+                              )).SingleOrDefault<data.Test>();
+            return task;
+        }
+
+        public List<data.Test> getAll()
+        {
+            IsFileExists();
+
+            XDocument doc = XDocument.Load(ConfigurationManager.AppSettings["TestsFile"]);
+            var tasks = (from T in doc.Root.Elements("test")
+                        select new data.Test
+                            (
+                               Int32.Parse(T.Attribute("id").Value),
+                                 T.Element("title").Value,
+                                 T.Element("desctiption").Value,
+                                 Int32.Parse(T.Attribute("categoriesId").Value),
+                                 DateTime.Parse(T.Element("creationDate").Value),
+                                 Int32.Parse(T.Attribute("authorId").Value),
+                                 Boolean.Parse(T.Element("isDraft").Value)
+                            )).ToList<data.Test>();
+            return tasks;
+        }
+
+        public void add(data.Test item)
+        {
+            IsFileExists();
+
+            XDocument doc = XDocument.Load(ConfigurationManager.AppSettings["TestsFile"]);
+            int maxId;
+            try
+            {
+                maxId = doc.Root.Elements("test").Max(t => Int32.Parse(t.Attribute("id").Value));
+            }
+            catch (Exception) { maxId = 0; }
+
+            XElement tests = new XElement("test",
+                new XAttribute("id", ++maxId),
+                new XElement("title", item.title),
+                new XElement("desctiption", item.title),
+                new XAttribute("categoriesId", item.title),
+                new XElement("creationDate", item.title),
+                new XAttribute("authorId", item.title),
+                new XElement("isDraft", item.title));
+            doc.Root.Add(tests);
+            doc.Save(ConfigurationManager.AppSettings["TestsFile"]);
+        }
+
+        public void update(data.Test item)
+        {
+            IsFileExists();
+
+            XDocument doc = XDocument.Load(ConfigurationManager.AppSettings["TestsFile"]);
+            var test = (from A in doc.Root.Elements("test")
+                          where Int32.Parse(A.Attribute("id").Value) == item.id
+                          select A).FirstOrDefault();
+            test.SetElementValue("title", item.title);
+            test.SetElementValue("desctiption", item.desctiption);
+            test.SetAttributeValue("categoriesId", item.categoriesId);
+            test.SetElementValue("creationDate", item.creationDate);
+            test.SetAttributeValue("authorId", item.authorId);
+            test.SetElementValue("isDraft", item.isDraft);
+            doc.Save(ConfigurationManager.AppSettings["TestsFile"]);
+        }
+
+        public void delete(data.Test item)
+        {
+            IsFileExists();
+
+            XDocument doc = XDocument.Load(ConfigurationManager.AppSettings["TestsFile"]);
+            var user = doc.Root.Descendants("test").Where(
+                   t => Int32.Parse(t.Attribute("id").Value) == item.id
+                ).ToList();
+            user.Remove();
+            doc.Save(ConfigurationManager.AppSettings["TestsFile"]);
+        }
+        static void IsFileExists()
+        {
+            if (!File.Exists(ConfigurationManager.AppSettings["TestsFile"]))
+            {
+                XNamespace empNM = "urn:lst-emp:emp";
+
+                XDocument xDoc = new XDocument(
+                        new XDeclaration("1.0", "UTF-16", null),
+                        new XElement(empNM + "tests"
+                                )
+                        );
+                StringWriter sw = new StringWriter();
+                XmlWriter xWrite = XmlWriter.Create(sw);
+                xDoc.Save(xWrite);
+                xWrite.Close();
+                xDoc.Save(ConfigurationManager.AppSettings["TestsFile"]);
+            }
+        }
+    }
+}
