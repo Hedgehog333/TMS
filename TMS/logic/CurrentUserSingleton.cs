78c036a0e8e3de71044c63b7f08132e97bfe7f68
Hedgehog333 <artem1100.94@gmail.com>
2016-05-31 03:01:56 +0300

    storage mechanism of the current user

---
+++ TMS/logic/CurrentUserSingleton.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMS.data;
+namespace TMS.logic
+{
+    public class CurrentUserSingleton
+    {
+        private User user;
+        public User User {
+            get
+            {
+                return this.user;
+            }
+        }
+        private CurrentUserSingleton()
+        {
+        }
+        public static CurrentUserSingleton Instance
+        {
+            get
+            {
+                if (InstanceHolper._instance.user == null)
+                    InstanceHolper._instance.user = LoginMethod.getUser();
+                return InstanceHolper._instance;
+            }
+        }
+        protected class InstanceHolper
+        {
+            static InstanceHolper() { }
+            internal static CurrentUserSingleton _instance = new CurrentUserSingleton();
+        }
+        /// <summary>
+        /// If needed logout.
+        /// </summary>
+        public static void Clear()
+        {
+            InstanceHolper._instance.user = null;
+        }
+    }
+}
---
+++ TMS/logic/LoginMethod.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS.logic
+{
+    /// <summary>
+    /// Needed to initialize the data CurrentUserSingleton.
+    /// </summary>
+    public class LoginMethod
+    {
+        private static  data.User _user;
+        public static data.User getUser()
+        {
+            return _user;
+        }
+        public LoginMethod(string email)
+        {
+            _user = UserDatabaseManagerSingleton.Instance.get(email);
+        }
+    }
+}
