fed188aefcd31d1228b72151f6da7e79d63ca75a
Hedgehog333 <artem1100.94@gmail.com>
2016-06-02 06:24:39 +0300

    add windows reate category & test

---
+++ TMS/model/CreateCategoryTest.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TMS.model
+{
+    /// <summary>
+    /// Interaction logic for CreateCategoryTest.xaml
+    /// </summary>
+    public partial class CreateCategoryTest : Window
+    {
+        public CreateCategoryTest()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGoBack_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnCreateCategory_Click(object sender, RoutedEventArgs e)
+        {
+            if (!String.IsNullOrWhiteSpace(this.txtbCategoryName.Text))
+                if (dao.Manager<db.XMLCategoriesDB>.Instance.get(this.txtbCategoryName.Text) == null)
+                {
+                    dao.Manager<db.XMLCategoriesDB>.Instance.add(new data.Categories(-1, this.txtbCategoryName.Text));
+                    MessageBox.Show("Save complite.");
+                    this.txtbCategoryName.Clear();
+                }
+                else
+                    MessageBox.Show("Category: \"" + this.txtbCategoryName.Text + "\" is already exist");
+            else
+                MessageBox.Show("Empty field!");
+        }
+    }
+}
---
+++ TMS/model/CreateTest.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using TMS.logic;
+
+namespace TMS.model
+{
+    /// <summary>
+    /// Interaction logic for CreateTest.xaml
+    /// </summary>
+    public partial class CreateTest : Window
+    {
+        public CreateTest()
+        {
+            InitializeComponent();
+
+            List<data.Categories> dataList = dao.Manager<db.XMLCategoriesDB>.Instance.getAll();
+            this.cmbbCategories.ItemsSource = dataList;
+            this.cmbbCategories.DisplayMemberPath = "title";
+            this.cmbbCategories.SelectedValuePath = "id";
+        }
+
+        private void btnCreate(object sender, RoutedEventArgs e)
+        {
+            if(!String.IsNullOrWhiteSpace(this.txtbTitle.Text) && this.cmbbCategories.SelectedValue != null)
+            {
+                this.txtbTitle.BorderBrush = Brushes.Green;
+                this.cmbbCategories.BorderBrush = Brushes.Green;
+                dao.Manager<db.XMLTestDB>.Instance.add(
+                    new data.Test(
+                        -1,
+                        this.txtbTitle.Text,
+                        this.txtbDescription.Text,
+                        (int)this.cmbbCategories.SelectedValue,
+                        DateTime.Now,
+                        DateTime.Now,
+                        CurrentUserSingleton.Instance.User.id,
+                        true
+                    )
+                );
+                MessageBox.Show("Save complite.");
+                this.txtbTitle.Clear();
+                this.txtbDescription.Clear();
+                this.cmbbCategories.SelectedValue = null;
+            }
+            else
+            {
+                if (String.IsNullOrWhiteSpace(this.txtbTitle.Text))
+                    this.txtbTitle.BorderBrush = Brushes.Red;
+                if(this.cmbbCategories.SelectedValue == null)
+                    this.cmbbCategories.BorderBrush = Brushes.Red;
+            }
+        }
+
+        private void btnGoBack(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
