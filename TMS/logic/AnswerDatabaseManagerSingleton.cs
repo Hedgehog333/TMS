c8582cc606628a201f75fc78e57baf32c26b4dd5
Hedgehog333 <artem1100.94@gmail.com>
2016-06-03 07:53:29 +0300

    add answer database maanger

---
+++ TMS/logic/AnswerDatabaseManagerSingleton.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS.logic
+{
+    /// <summary>
+    /// There is determined a database to work.
+    /// </summary>
+    class AnswerDatabaseManagerSingleton
+    {
+        private AnswerDatabaseManagerSingleton()
+        {}
+        public static db.XMLAnswerDB Instance
+        {
+            get
+            {
+                return dao.Manager<db.XMLAnswerDB>.Instance;
+            }
+        }
+    }
+}
