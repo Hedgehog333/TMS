464f35039e6634c1f6dc43a0618d4b3f0fb2f4c1
Hedgehog333 <artem1100.94@gmail.com>
2016-06-03 21:46:07 +0300

    class binding showResults form

---
+++ TMS/data/ResultViewBindingDataGrid.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS.data
+{
+    public class ResultViewBindingDataGrid
+    {
+        public string TestName { get; set; }
+        public string UserName { get; set; }
+        public string UserLastname { get; set; }
+        public string UserGroup { get; set; }
+        public int correctQuestion { set; get; }
+        public int totalQuestion { set; get; }
+        public DateTime compliteTest { set; get; }
+    }
+}
