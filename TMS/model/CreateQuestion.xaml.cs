321bf77f9a45ab96218a1ea326310e534f83320d
Hedgehog333 <artem1100.94@gmail.com>
2016-06-03 07:42:33 +0300

    form create question

---
+++ TMS/model/CreateQuestion.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TMS.model
+{
+    /// <summary>
+    /// Interaction logic for CreateQuestion.xaml
+    /// </summary>
+    public partial class CreateQuestion : Window
+    {
+        private int idTest, QuestionId;
+        bool isEdit = false;
+        public CreateQuestion(int idTest)
+        {
+            InitializeComponent();
+            this.idTest = idTest;
+        }
+        public CreateQuestion(
+                int id,
+                string body,
+                int testId,
+                bool isFowAnswers,
+                bool isDraft
+            )
+        {
+            InitializeComponent();
+            isEdit = true;
+            this.txtbBody.Text = body;
+            this.idTest = testId;
+            this.QuestionId = id;
+            this.checkbIsFowAnswer.IsChecked = isFowAnswers;
+            this.checkbIsDraft.IsChecked = isDraft;
+        }
+
+        private void btnGoBack_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.isEdit)
+            {
+                dao.Manager<db.XMLQuestionDB>.Instance.update(new data.Question
+                    (
+                        this.QuestionId,
+                        this.txtbBody.Text,
+                        this.idTest,
+                        this.checkbIsFowAnswer.IsChecked.Value,
+                        this.checkbIsDraft.IsChecked.Value
+                    )
+                );
+                MessageBox.Show("Save complite.");
+                this.Close();
+            }
+            else
+            {
+                if (!String.IsNullOrWhiteSpace(this.txtbBody.Text))
+                {
+                    dao.Manager<db.XMLQuestionDB>.Instance.add(new data.Question
+                        (
+                            -1,
+                            this.txtbBody.Text,
+                            this.idTest,
+                            this.checkbIsFowAnswer.IsChecked.Value,
+                            this.checkbIsDraft.IsChecked.Value
+                        )
+                     );
+                    MessageBox.Show("Save complite.");
+                    this.txtbBody.Clear();
+                    this.checkbIsFowAnswer.IsChecked = false;
+                    this.checkbIsDraft.IsChecked = false;
+                }
+            }
+        }
+    }
+}
