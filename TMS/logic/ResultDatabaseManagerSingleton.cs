e34856f1e7aad9d78b37d3ea850a7a16558f8286
Hedgehog333 <artem1100.94@gmail.com>
2016-06-03 08:03:15 +0300

    add result database maanger

---
+++ TMS/logic/ResultDatabaseManagerSingleton.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS.logic
+{
+    /// <summary>
+    /// There is determined a database to work.
+    /// </summary>
+    class ResultDatabaseManagerSingleton
+    {
+        private ResultDatabaseManagerSingleton()
+        {}
+        public static db.XMLResultDB Instance
+        {
+            get
+            {
+                return dao.Manager<db.XMLResultDB>.Instance;
+            }
+        }
+    }
+}
