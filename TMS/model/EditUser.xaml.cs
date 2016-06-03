0b44ae864a4d58719415d0aea6f84760cf000da0
Hedgehog333 <artem1100.94@gmail.com>
2016-06-04 00:03:15 +0300

    add show and edit user

---
+++ TMS/model/EditUser.xaml.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using TMS.logic;
+
+namespace TMS.model
+{
+    /// <summary>
+    /// Interaction logic for EditUser.xaml
+    /// </summary>
+    public partial class EditUser : Window
+    {
+        private data.User user;
+        public EditUser(int id)
+        {
+            InitializeComponent();
+            this.user = UserDatabaseManagerSingleton.Instance.get(id);
+
+            this.txtbFName.Text = this.user.fName;
+            this.txtbLName.Text = this.user.lName;
+            this.txtbSName.Text = this.user.sName;
+            this.txtbEmail.Text = this.user.email;
+
+            if (CurrentUserSingleton.Instance.User.id == id)
+            {
+                this.cboxRole.SelectedIndex = 0;
+                this.cboxRole.IsEnabled = false;
+            }
+            else
+                this.cboxRole.SelectedIndex = (int)user.role;
+            this.cboxGroup.ItemsSource = GroupDatabaseManagerSingleton.Instance.getAll();
+            this.cboxGroup.DisplayMemberPath = "Name";
+            this.cboxGroup.SelectedValuePath = "id";
+
+            this.cboxGroup.SelectedValue = GroupDatabaseManagerSingleton.Instance.get(user.groupId).id;
+        }
+
+        private void btnGoBackClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnSignUpClick(object sender, RoutedEventArgs e)
+        {
+            bool fN = false, lN = fN, em = fN, pas = fN, gbR = fN, gbN = fN;
+
+            if (String.IsNullOrWhiteSpace(this.txtbFName.Text))
+                this.txtbFName.BorderBrush = Brushes.Red;
+            else
+            {
+                this.txtbFName.BorderBrush = Brushes.Green;
+                fN = true;
+            }
+
+            if (String.IsNullOrWhiteSpace(this.txtbLName.Text))
+                this.txtbLName.BorderBrush = Brushes.Red;
+            else
+            {
+                this.txtbLName.BorderBrush = Brushes.Green;
+                lN = true;
+            }
+
+            if (String.IsNullOrWhiteSpace(this.txtbEmail.Text))
+                this.txtbEmail.BorderBrush = Brushes.Red;
+            else
+            {
+                this.txtbEmail.BorderBrush = Brushes.Green;
+                em = true;
+            }
+
+            if (String.IsNullOrWhiteSpace(this.pasPassword.Password))
+                this.pasPassword.BorderBrush = Brushes.Red;
+            else
+            {
+                this.pasPassword.BorderBrush = Brushes.Green;
+                pas = true;
+            }
+
+            if (this.cboxGroup.SelectedValue == null)
+                this.cboxGroup.BorderBrush = Brushes.Red;
+            else
+            {
+                this.cboxGroup.BorderBrush = Brushes.Green;
+                gbN = true;
+            }
+
+            if (this.cboxRole.SelectedValue == null)
+                this.cboxRole.BorderBrush = Brushes.Red;
+            else
+            {
+                this.cboxRole.BorderBrush = Brushes.Green;
+                gbR = true;
+            }
+
+            if (fN && lN && em && pas && gbR && gbN)
+            {  
+                UserDatabaseManagerSingleton.Instance.update(
+                    new data.User(
+                        user.id,
+                        this.txtbFName.Text,
+                        this.txtbLName.Text,
+                        this.txtbSName.Text,
+                        this.txtbEmail.Text,
+                        this.pasPassword.Password,
+                        (int)this.cboxGroup.SelectedValue,
+                        (data.ERoles)this.cboxRole.SelectedIndex,
+                        user.registrationDate
+                    )
+                );
+                Console.WriteLine(this.cboxRole.SelectedIndex);
+                MessageBox.Show("Save complite.");
+                this.Close();
+            }
+        }
+    }
+}
---
+++ TMS/model/ShowUsers.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using TMS.logic;
+
+namespace TMS.model
+{
+    /// <summary>
+    /// Interaction logic for ShowUsers.xaml
+    /// </summary>
+    public partial class ShowUsers : Window
+    {
+        public ShowUsers()
+        {
+            InitializeComponent();
+            RefreshUseers();
+        }
+        private void RefreshUseers()
+        {
+
+            List<data.User> users = UserDatabaseManagerSingleton.Instance.getAll();
+
+
+            this.spListUsers.Children.Clear();
+            foreach (data.User item in users)
+            {
+                Grid grid = new Grid()
+                {
+                    Uid = item.id.ToString(),
+                    MinHeight = 80,
+
+                };
+                ColumnDefinition gridCol1 = new ColumnDefinition()
+                {
+                    MinWidth = 150,
+                    Width = new GridLength(200, GridUnitType.Star)
+                };
+                ColumnDefinition gridCol2 = new ColumnDefinition()
+                {
+                    Width = new GridLength(74, GridUnitType.Star)
+                };
+                grid.ColumnDefinitions.Add(gridCol1);
+                grid.ColumnDefinitions.Add(gridCol2);
+
+                TextBlock bodyAnswer = new TextBlock()
+                {
+                    Text = item.fName + " " + item.lName,
+                    Margin = new Thickness(10, 10, 10, 0),
+                    TextWrapping = TextWrapping.WrapWithOverflow,
+                    VerticalAlignment = System.Windows.VerticalAlignment.Top,
+                };
+                grid.Children.Add(bodyAnswer);
+
+                Button edit = new Button()
+                {
+                    Content = "Edit",
+                    HorizontalAlignment = System.Windows.HorizontalAlignment.Left,
+                    VerticalAlignment = System.Windows.VerticalAlignment.Top,
+                    Height = 33,
+                    Width = 75,
+                    Margin = new Thickness(10, 5, 0, 0)
+                };
+                Grid.SetColumn(edit, 1);
+                edit.Click += this.btnEditUser_Click;
+
+                grid.Children.Add(edit);
+
+
+                Button delete = new Button()
+                {
+                    Content = "Delete",
+                    HorizontalAlignment = System.Windows.HorizontalAlignment.Left,
+                    VerticalAlignment = System.Windows.VerticalAlignment.Top,
+                    Height = 33,
+                    Width = 75,
+                    Margin = new Thickness(10, 42, 0, 0),
+                    IsEnabled = item.id == CurrentUserSingleton.Instance.User.id ? false : true
+                };
+                Grid.SetColumn(delete, 1);
+                delete.Click += this.btnDeleteUser_Click;
+
+                grid.Children.Add(delete);
+
+
+                this.spListUsers.Children.Add(grid);
+            }
+        }
+
+        private void btnDeleteUser_Click(object sender, RoutedEventArgs e)
+        {
+            int id = Convert.ToInt32(((sender as Button).Parent as Grid).Uid);
+            UserDatabaseManagerSingleton.Instance.delete(UserDatabaseManagerSingleton.Instance.get(id));
+            this.RefreshUseers();
+        }
+
+        private void btnEditUser_Click(object sender, RoutedEventArgs e)
+        {
+            int id = Convert.ToInt32(((sender as Button).Parent as Grid).Uid);
+            EditUser ES = new EditUser(id);
+            ES.ShowDialog();
+            this.RefreshUseers();
+        }
+
+        private void btnGoBack_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
