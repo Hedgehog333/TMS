38be9fb0efe0559e33241cc1501618f126d12882
Hedgehog333 <artem1100.94@gmail.com>
2016-06-03 07:55:39 +0300

    add question database maanger

---
+++ TMS/logic/QuestionDatabaseManagerSingleton.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS.logic
+{
+    class QuestionDatabaseManagerSingleton
+    {
+        private QuestionDatabaseManagerSingleton()
+        {}
+        public static db.XMLQuestionDB Instance
+        {
+            get
+            {
+                return dao.Manager<db.XMLQuestionDB>.Instance;
+            }
+        }
+    }
+}
