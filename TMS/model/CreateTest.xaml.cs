e06121953bc1fd3be7322cbaed6561d9dbe0c019
Hedgehog333 <artem1100.94@gmail.com>
2016-06-03 08:35:57 +0300

    add edit constructor and operation

--- TMS/model/CreateTest.xaml.cs
+++ TMS/model/CreateTest.xaml.cs
@@ -29,25 +29,74 @@ namespace TMS.model
             this.cmbbCategories.DisplayMemberPath = "title";
             this.cmbbCategories.SelectedValuePath = "id";
         }
+        int TestId, AuthorId;
+        bool isEdit = false;
+        DateTime CreationDate;
+        public CreateTest
+            (
+                int id,
+                string title,
+                string desctiption,
+                int categoriesId,
+                int authorId,
+                DateTime crete,
+                bool isDraft
+            )
+        {
+            InitializeComponent();
+
+            List<data.Categories> dataList = dao.Manager<db.XMLCategoriesDB>.Instance.getAll();
+            this.cmbbCategories.ItemsSource = dataList;
+            this.cmbbCategories.DisplayMemberPath = "title";
+            this.cmbbCategories.SelectedValuePath = "id";
+
+            this.isEdit = true;
+            this.TestId = id;
+            this.AuthorId = authorId;
+            this.txtbTitle.Text = title;
+            this.txtbDescription.Text = desctiption;
+            this.cmbbCategories.SelectedValue = categoriesId;
+            this.checkbIsDraft.IsChecked = isDraft;
+            this.CreationDate = crete;
+        }
 
         private void btnCreate(object sender, RoutedEventArgs e)
         {
-            if(!String.IsNullOrWhiteSpace(this.txtbTitle.Text) && this.cmbbCategories.SelectedValue != null)
+            if (!String.IsNullOrWhiteSpace(this.txtbTitle.Text) && this.cmbbCategories.SelectedValue != null)
             {
                 this.txtbTitle.BorderBrush = Brushes.Green;
                 this.cmbbCategories.BorderBrush = Brushes.Green;
-                dao.Manager<db.XMLTestDB>.Instance.add(
-                    new data.Test(
-                        -1,
-                        this.txtbTitle.Text,
-                        this.txtbDescription.Text,
-                        (int)this.cmbbCategories.SelectedValue,
-                        DateTime.Now,
-                        DateTime.Now,
-                        CurrentUserSingleton.Instance.User.id,
-                        true
-                    )
-                );
+                if (isEdit)
+                {
+                    dao.Manager<db.XMLTestDB>.Instance.update(new data.Test
+                        (
+                            this.TestId,
+                            this.txtbTitle.Text,
+                            this.txtbDescription.Text,
+                            (int)this.cmbbCategories.SelectedValue,
+                            this.CreationDate,
+                            DateTime.Now,
+                            this.AuthorId,
+                            this.checkbIsDraft.IsChecked.Value
+                        ));
+                    MessageBox.Show("Save complite.");
+                    this.Close();
+                }
+                else
+                {
+                    dao.Manager<db.XMLTestDB>.Instance.add(
+                        new data.Test(
+                            -1,
+                            this.txtbTitle.Text,
+                            this.txtbDescription.Text,
+                            (int)this.cmbbCategories.SelectedValue,
+                            DateTime.Now,
+                            DateTime.Now,
+                            CurrentUserSingleton.Instance.User.id,
+                            this.checkbIsDraft.IsChecked.Value
+                        )
+                    );
+                }
                 MessageBox.Show("Save complite.");
                 this.txtbTitle.Clear();
                 this.txtbDescription.Clear();
@@ -57,9 +106,10 @@ namespace TMS.model
             {
                 if (String.IsNullOrWhiteSpace(this.txtbTitle.Text))
                     this.txtbTitle.BorderBrush = Brushes.Red;
-                if(this.cmbbCategories.SelectedValue == null)
+                if (this.cmbbCategories.SelectedValue == null)
                     this.cmbbCategories.BorderBrush = Brushes.Red;
             }
+            
         }
 
         private void btnGoBack(object sender, RoutedEventArgs e)
