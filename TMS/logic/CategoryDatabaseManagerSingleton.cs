0afd6b2688c66978b7dfcdc38bc5823d9d9331cb
Hedgehog333 <artem1100.94@gmail.com>
2016-06-03 08:02:40 +0300

    add category database maanger

---
+++ TMS/logic/CategoryDatabaseManagerSingleton.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS.logic
+{
+    /// <summary>
+    /// There is determined a database to work.
+    /// </summary>
+    class CategoryDatabaseManagerSingleton
+    {
+        private CategoryDatabaseManagerSingleton()
+        {}
+        public static db.XMLCategoriesDB Instance
+        {
+            get
+            {
+                return dao.Manager<db.XMLCategoriesDB>.Instance;
+            }
+        }
+    }
+}
